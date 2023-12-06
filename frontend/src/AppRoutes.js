9325fff2980670adc356d0c586544bdf473325e0
hashini-fernando <139649940+hashini-fernando@users.noreply.github.com>
2023-12-07 00:11:25 +0530

    Update and rename Pages to AppRoutes.js

---
+++ frontend/src/AppRoutes.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import {Route,Routes } from 'react-router-dom'
+import { Login } from './Pages/Login';
+import { Register } from './Pages/Register';
+import Dashboard from './Pages/Dashboard';
+
+export default function AppRoute() {
+    return ( 
+    <Routes>
+      <Route path ='/'element = {<Login/>}/>
+      <Route path ='/register'element = {<Register/>}/>
+      <Route path ='/dashboard'element = {<Dashboard/>}/>
+
+    </Routes>
+
+    );
+  }
