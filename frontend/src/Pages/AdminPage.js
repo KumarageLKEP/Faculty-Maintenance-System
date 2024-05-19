a32d49c8cf11ad6b4faaefaac4da04e934a86a4c
KavindaSandamal <info.kavindasandamal@gmail.com>
2024-05-19 10:44:00 +0530

    update adminpage

--- frontend/src/Pages/AdminPage.js
+++ frontend/src/Pages/AdminPage.js
@@ -1,11 +1,13 @@
 import React, { useState, useEffect } from 'react';
 import axios from 'axios';
+import { useNavigate } from 'react-router-dom';
 
 function AdminPage() {
+  const navigate = useNavigate();
   const [requests, setRequests] = useState([]);
+  const [error, setError] = useState(null);
 
   useEffect(() => {
-    // Fetch requests with inactive status from the backend upon component mounting
     fetchRequests();
   }, []);
 
@@ -17,49 +19,108 @@ function AdminPage() {
       setRequests(inactiveRequests);
     } catch (error) {
       console.error('Error fetching requests:', error);
+      setError('Error fetching requests');
     }
   };
 
   const handleApprove = async (userId) => {
     try {
       await axios.put(`http://localhost:8000/user/approve/${userId}`);
-      // Refresh the requests list after approving
       fetchRequests();
     } catch (error) {
       console.error('Error approving request:', error);
+      setError('Error approving request');
     }
   };
 
   const handleReject = async (userId) => {
     try {
       await axios.delete(`http://localhost:8000/user/delete/${userId}`);
-      // Refresh the requests list after rejecting
       fetchRequests();
     } catch (error) {
       console.error('Error rejecting request:', error);
+      setError('Error rejecting request');
     }
   };
 
   return (
-    <div className='main-container'>
-      <h1>Admin Page</h1>
-      <div className='requests-list'>
-        {requests.map(request => (
-          <div key={request._id} className='request-item'>
-            <p>{request.fullName}</p>
-            <p>{request.email}</p>
-            <p>{request.regNo}</p>
-            <p>{request.contactNumber}</p>
-            <p>Status: {request.status}</p>
-            {request.status === 'inactive' && (
-              <div>
-                <button onClick={() => handleApprove(request._id)}>Approve</button>
-                <button onClick={() => handleReject(request._id)}>Reject</button>
-              </div>
-            )}
-          </div>
-        ))}
+    <div className="container-fluid">
+      <div className="d-flex justify-content-between align-items-center py-3">
+        <h2 className="h5 mb-0"><a href="#" className="text-muted"></a> Admin Dashboard</h2>
       </div>
+      {requests.map(request => (
+        <div className="row">
+          <div className="col-lg-8">
+            <div key={request._id} className="card mb-4">
+              <div className="card-body">
+                <div className="mb-3 d-flex justify-content-between">
+                  <div>
+                    <span className="me-3">User Details</span>
+                  </div>
+                </div>
+                <table className="table table-borderless">
+                  <tbody>
+                    <tr>
+                      <td>
+                        <div className="d-flex mb-2">
+                          <div className="flex-lg-grow-1 ms-3">
+                            <h6 className="small mb-0">Name</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="text-end"> {request.fullName}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div className="d-flex mb-2">
+                          <div className="flex-lg-grow-1 ms-3">
+                            <h6 className="small mb-0">Email</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="text-end" >{request.email}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div className="d-flex mb-2">
+                          <div className="flex-lg-grow-1 ms-3">
+                            <h6 className="small mb-0">UserName</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td  className="text-end">{request.regNo}</td>
+                    </tr>
+                    <tr>
+                      <td>
+                        <div className="d-flex mb-2">
+                          <div className="flex-lg-grow-1 ms-3">
+                            <h6 className="small mb-0">Contact Number</h6>
+                          </div>
+                        </div>
+                      </td>
+                      <td className="text-end">{request.contactNumber}</td>
+                    </tr>
+                  </tbody>
+                </table>
+              </div>
+            </div>
+          </div>
+          <div className="col-lg-4">
+            <div className="card mb-4">
+              <div className="card-body">
+                <h3 className="h6">Status</h3>
+                <p>{request.status}</p>
+                {request.status === 'inactive' && (
+                  <div>
+                    <button className="btn btn-secondary btn-sm" onClick={() => handleApprove(request._id)}>Approve</button>
+                    <button className="btn btn-secondary btn-sm" style={{ marginLeft: '10px' }} onClick={() => handleReject(request._id)}>Reject</button>
+                  </div>
+                )}
+              </div>
+            </div>
+          </div>
+        </div>
+      ))}
     </div>
   );
 }
